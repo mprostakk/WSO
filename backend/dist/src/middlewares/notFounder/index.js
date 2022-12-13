a72e985dd5b4ec05f5ab6c6ea4849293b48ec433
Adrian <44926230+daddyaddy@users.noreply.github.com>
2022-12-13 21:39:55 +0100

    Remove dist

--- backend/dist/src/middlewares/notFounder/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const notFounder = async (err, req, res, next) => {
-    next(new Error(`Not found`));
-};
-exports.default = notFounder;
-//# sourceMappingURL=index.js.map
