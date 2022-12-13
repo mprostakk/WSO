060792c29fe47f4d9587ef63eadc13a4f2cf8f26
Maciej Prostak <maciej.prostak@Choreograph.com>
2022-12-13 16:52:51 +0100

    Add example piston request

--- README.md
+++ README.md
@@ -6,6 +6,38 @@ Simple code execution system with tasks and submissions
 
 `docker-compose up --build`
 
+## Installing language on Piston
+```
+POST localhost:9000/api/v2/packages
+
+{
+    "language": "python",
+    "version": "3.9.4"
+}
+```
+
+## Example execute request to Piston
+```
+POST localhost:9000/api/v2/execute
+
+{
+    "language": "python",
+    "version": "3.9.4",
+    "files": [
+        {
+            "name": "cool.py",
+            "content": "print(\"Hello world\")"
+        }
+    ],
+    "stdin": "",
+    "args": [],
+    "compile_timeout": 10000,
+    "run_timeout": 3000,
+    "compile_memory_limit": -1,
+    "run_memory_limit": -1
+}
+```
+
 ## Technologies
 - Piston `https://github.com/engineer-man/piston`
 - Piston node client `https://github.com/dthree/node-piston`
--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,3 +1,4 @@
+import axios from "axios";
 import express, { Express, Request, Response } from "express";
 import apiRouter from "./api";
 
@@ -8,6 +9,28 @@ app.use("/api", apiRouter);
 
 app.get("/", async (req: Request, res: Response) => {
   res.status(200).json({ status: "success" });
+});
+
+app.get("/run", async (req: Request, res: Response) => {
+  const data = {
+    "language": "python",
+    "version": "3.9.4",
+    "files": [
+        {
+            "name": "run.py",
+            "content": "print(\"Hello world\")"
+        }
+    ],
+    "stdin": "",
+    "args": [],
+    "compile_timeout": 10000,
+    "run_timeout": 3000,
+    "compile_memory_limit": -1,
+    "run_memory_limit": -1
+  }
+  const response = await axios.post("http://piston:2000/api/v2/execute", data)
+
+  res.status(200).json({ status: "success", pistonResponse: response.data });
 });
 
 apiRouter.get("/healthcheck", (req, res, next) => {
